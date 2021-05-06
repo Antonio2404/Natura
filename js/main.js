ed95f4c6c706c3d2666bad741964235175941d4f
Antonio2404 <azorinn@list.ru>
2021-05-06 23:29:27 +0300

    Update header 4

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(function () {
+//поиск
+    $('.searchingButton').click(function(){
+        $('.showMenuSearching').toggleClass('show-menu')
+    })
+
+//меню 
+    $('.menuButton').click(function(){
+        $('.showMenuButton').toggleClass('show-menu')
+    })
+
+    
+})
