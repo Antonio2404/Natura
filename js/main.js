8734497340dd5d85148c056d5158c56020c32446
Antonio2404 <azorinn@list.ru>
2021-05-16 00:43:56 +0300

    Футер

--- js/main.js
+++ js/main.js
@@ -1,13 +1,22 @@
-$(function () {
-//поиск
-    $('.searchingButton').click(function(){
-        $('.showMenuSearching').toggleClass('show-menu')
-    })
+// $(function () {
+// //поиск
+//     $('.searchingButton').click(function(){
+//         $('.showMenuSearching').toggleClass('show-menu')
+//     })
 
-//меню 
-    $('.menuButton').click(function(){
-        $('.showMenuButton').toggleClass('show-menu')
-    })
+// //меню 
+//     $('.menuButton').click(function(){
+//         $('.showMenuButton').toggleClass('show-menu')
+//     })
 
     
-})
+// })
+
+document.addEventListener("DOMContentLoaded", () => {
+    const searchingButton = document.querySelector('.searchingButton');
+    const showMenuSearching = document.querySelector('.showMenuSearching');
+
+    searchingButton.addEventListener('click', function () {
+        showMenuSearching.classList.toggle('show-menu');
+    })
+});
